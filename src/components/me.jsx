95734c555c99aa72080e57a4747ff6e15e3ffb3e
Akainu <akainu@localhost>
2024-11-29 22:59:41 +0100

    Navbar

---
+++ src/components/me.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import NavBar from './navBar';
+
+class ME extends Component {
+    state = {  } 
+
+constructor (){
+    super();
+}
+
+    render() { 
+        return (
+            <NavBar/>
+            
+        );
+    }
+}
+ 
+export default ME;
---
+++ src/components/navBar.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+  state = {
+    count:10,
+    
+    
+  
+    } 
+  render() { 
+    return (
+      
+      <div>
+        <h2>me mem</h2>
+        <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptate, ut fuga dicta totam enim repudiandae reprehenderit corrupti voluptatibus, deleniti doloremque labore esse? Magni aut modi inventore officiis numquam beatae a quidem neque, praesentium cum. Totam perferendis eligendi dicta fugit, sapiente suscipit voluptatem, tenetur tempora odit iure tempore quis hic exercitationem at consequuntur aperiam cupiditate! Velit quod ea magnam odit, est hic cupiditate repellendus quisquam quos expedita sequi similique voluptate ad illum molestiae alias iste!</h1>
+        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime error vel officia quo iure ad corrupti, tempora quaerat aperiam voluptatum, architecto corporis facilis quas beatae iste molestiae a atque nobis? Alias, expedita aliquam aliquid architecto corporis magni, voluptatibus id similique illum ullam sint ut delectus natus dolore explicabo praesentium quibusdam eum cumque, accusantium perspiciatis accusamus aperiam veniam perferendis? Modi ad nam earum sapiente veniam eius minima repudiandae vero soluta et neque perferendis cum sit, incidunt molestias praesentium aperiam assumenda culpa.</h2>
+      </div>
+    );
+  }
+}
+ 
+export default NavBar;
